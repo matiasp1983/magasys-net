a4097f465564c9915c7df68f2b535e9819f9d748
matiasp1983 <matiasp1983@gmail.com>
2013-08-07 14:31:57 +0000

    Se cambiaron las Propiedades y COnstructor utilizando Herencia de la clase Producto

--- Magasys/Dyn.Database/entities/Coleccion.cs
+++ Magasys/Dyn.Database/entities/Coleccion.cs
@@ -5,24 +5,16 @@ using System.Text;
 
 namespace Dyn.Database.entities
 {
-    public class Coleccion
+    public class Coleccion : Producto
     {
         #region Constructores
 
         public Coleccion() { }
 
-        public Coleccion(Int32? idCol, DateTime? fechcreac, string nomb, string descrip, Int16? est,
-            Int32? cantidad, Int32? idTiposProd, Int32? idGen, Int32? idProv, Int32? idPeriod, Double? prec, Int16? cantEnt)
+        public Coleccion(Int32? idGen, Int32? idPeriod, Double? prec, Int16? cantEnt)
         {
-            idColeccion = idCol;
-            fechacreacion = fechcreac;
-            nombre = nomb;
-            descripcion = descrip;
-            estado = est;
-            cantidaddisponible = cantidad;
-            idTiposProducto = idTiposProd;
+            idColeccion = base.IdProducto;
             idGenero = idGen;
-            idProveedor = idProv;
             idPeriodicidad = idPeriod;
             precio = prec;
             cantidadEntrega = cantEnt;
@@ -33,91 +25,20 @@ namespace Dyn.Database.entities
         #region Propiedades
 
         private Int32? idColeccion;
-
         public Int32? IdColeccion
         {
             get { return idColeccion; }
             set { idColeccion = value; }
         }
 
-        private DateTime? fechacreacion;
-
-        public DateTime? Fechacreacion
-        {
-            get { return fechacreacion; }
-            set { fechacreacion = value; }
-        }
-
-        private string nombre;
-
-        public string Nombre
-        {
-            get { return nombre; }
-            set { nombre = value; }
-        }
-
-        private string descripcion;
-
-        public string Descripcion
-        {
-            get { return descripcion; }
-            set { descripcion = value; }
-        }
-
-        private Int16? estado = null;
-
-        public enum GeneroEstado
-        {
-            Activo = 1,
-            Inactivo = 0
-        }
-
-        public GeneroEstado Estado
-        {
-            get
-            {
-                return (GeneroEstado)estado;
-            }
-            set
-            {
-                estado = (Int16)value;
-            }
-        }
-
-        private Int32? cantidaddisponible;
-
-        public Int32? Cantidaddisponible
-        {
-            get { return cantidaddisponible; }
-            set { cantidaddisponible = value; }
-        }
-
-        private Int32? idTiposProducto;
-
-        public Int32? IdTiposProducto
-        {
-            get { return idTiposProducto; }
-            set { idTiposProducto = value; }
-        }
-
         private Int32? idGenero;
-
         public Int32? IdGenero
         {
             get { return idGenero; }
             set { idGenero = value; }
         }
 
-        private Int32? idProveedor;
-
-        public Int32? IdProveedor
-        {
-            get { return idProveedor; }
-            set { idProveedor = value; }
-        }
-
         private Int32? idPeriodicidad;
-
         public Int32? IdPeriodicidad
         {
             get { return idPeriodicidad; }
@@ -125,7 +46,6 @@ namespace Dyn.Database.entities
         }
 
         private Double? precio;
-
         public Double? Precio
         {
             get { return precio; }
@@ -133,7 +53,6 @@ namespace Dyn.Database.entities
         }
 
         private Int16? cantidadEntrega;
-
         public Int16? CantidadEntrega
         {
             get { return cantidadEntrega; }
