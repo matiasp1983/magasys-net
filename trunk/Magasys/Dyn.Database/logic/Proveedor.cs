e8fc61ed5ed432535cfbef44df687ae9255662d8
matiasp1983@gmail.com <matiasp1983@gmail.com@267969d2-7f81-b237-fc1d-be3311caf4fb>
2013-08-08 20:50:44 +0000

    Modificacion en Logica Proveedor

--- trunk/Magasys/Dyn.Database/logic/Proveedor.cs
+++ trunk/Magasys/Dyn.Database/logic/Proveedor.cs
@@ -20,5 +20,28 @@ namespace Dyn.Database.logic
             }
             return Collection;
         }
+        public DataSet SeleccionarProveedorPorNombrePaginadoAdmin(string nombre, int paginaactual, ref int numeropaginas)
+        {
+            CreateCommand("usp_SeleccionarGenerosPorNombrePaginado", true);
+            AddCmdParameter("@nombre", nombre, ParameterDirection.Input);
+            AddCmdParameter("@CurrentPage", paginaactual, ParameterDirection.Input);
+            AddCmdParameter("@PageSize", 100, ParameterDirection.Input);
+            AddCmdParameter("@TotalRecords", ParameterDirection.Output);
+            DataSet ds = GetDataSet();
+            numeropaginas = (int)GetValueCmdParameter("@TotalRecords");
+            return ds;
+        }
+        public List<Dyn.Database.entities.Genero> SeleccionarTodosLosGeneros()
+        {
+            List<Dyn.Database.entities.Genero> Collection = new List<Dyn.Database.entities.Genero>();
+            CreateCommand("usp_SeleccionarTodosLosGeneros", true);
+            ExecuteReader();
+            while (Read())
+            {
+                Collection.Add(new Dyn.Database.entities.Genero(GetDataReader()));
+            }
+            return Collection;
+        }
+
     }
 }
