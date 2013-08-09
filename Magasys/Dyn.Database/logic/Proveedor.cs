36a68cfb3aa381a1c482611357326155b155e436
matiasp1983 <matiasp1983@gmail.com>
2013-08-09 01:21:48 +0000

    Logica Proveedores, ultima Actualizacion

--- Magasys/Dyn.Database/logic/Proveedor.cs
+++ Magasys/Dyn.Database/logic/Proveedor.cs
@@ -9,21 +9,35 @@ namespace Dyn.Database.logic
     {
         public Proveedor() { }
 
-        public List<Dyn.Database.entities.Proveedor> SeleccionarTodosLosProveedores()
+        public Dyn.Database.entities.Proveedor Load(int idProveedor)
         {
-            List<Dyn.Database.entities.Proveedor> Collection = new List<Dyn.Database.entities.Proveedor>();
-            CreateCommand("usp_SeleccionarTodosLosProveedores", true);
+            Dyn.Database.entities.Proveedor objProveedor = new Dyn.Database.entities.Proveedor();
+            CreateCommand("usp_Proveedor", true);
+            AddCmdParameter("@Proveedor", idProveedor, ParameterDirection.Input);
+            AddCmdParameter("@Action", 0, ParameterDirection.Input);
             ExecuteReader();
             while (Read())
-            {                
-                Collection.Add(new Dyn.Database.entities.Proveedor(GetDataReader()));
+            {
+                objProveedor = new Dyn.Database.entities.Proveedor(GetDataReader());
             }
-            return Collection;
+            return objProveedor;
         }
-        public DataSet SeleccionarProveedorPorNombrePaginadoAdmin(string nombre, int paginaactual, ref int numeropaginas)
+
+        //public List<Dyn.Database.entities.Proveedor> SeleccionarTodosLosProveedores()
+        //{
+        //    List<Dyn.Database.entities.Proveedor> Collection = new List<Dyn.Database.entities.Proveedor>();
+        //    CreateCommand("usp_SeleccionarTodosLosProveedores", true);
+        //    ExecuteReader();
+        //    while (Read())
+        //    {                
+        //        Collection.Add(new Dyn.Database.entities.Proveedor(GetDataReader()));
+        //    }
+        //    return Collection;
+        //}
+        public DataSet SeleccionarProveedorPorNombrePaginadoAdmin(string razonSocial, int paginaactual, ref int numeropaginas)
         {
-            CreateCommand("usp_SeleccionarGenerosPorNombrePaginado", true);
-            AddCmdParameter("@nombre", nombre, ParameterDirection.Input);
+            CreateCommand("usp_SeleccionarProveedoresPorNombrePaginado", true);
+            AddCmdParameter("@razonSocial", razonSocial, ParameterDirection.Input);
             AddCmdParameter("@CurrentPage", paginaactual, ParameterDirection.Input);
             AddCmdParameter("@PageSize", 100, ParameterDirection.Input);
             AddCmdParameter("@TotalRecords", ParameterDirection.Output);
@@ -31,16 +45,67 @@ namespace Dyn.Database.logic
             numeropaginas = (int)GetValueCmdParameter("@TotalRecords");
             return ds;
         }
-        public List<Dyn.Database.entities.Genero> SeleccionarTodosLosGeneros()
+        //public List<Dyn.Database.entities.Genero> SeleccionarTodosLosGeneros()
+        //{
+        //    List<Dyn.Database.entities.Genero> Collection = new List<Dyn.Database.entities.Genero>();
+        //    CreateCommand("usp_SeleccionarTodosLosGeneros", true);
+        //    ExecuteReader();
+        //    while (Read())
+        //    {
+        //        Collection.Add(new Dyn.Database.entities.Genero(GetDataReader()));
+        //    }
+        //    return Collection;
+        //}
+
+        private void AddParameters(Dyn.Database.entities.Proveedor objusuario)
         {
-            List<Dyn.Database.entities.Genero> Collection = new List<Dyn.Database.entities.Genero>();
-            CreateCommand("usp_SeleccionarTodosLosGeneros", true);
+            CreateCommand("usp_Proveedor", true);
+            AddCmdParameter("@idProveedor", objusuario.IdProveedor, ParameterDirection.Input);
+            AddCmdParameter("@cuit", objusuario.Cuit, ParameterDirection.Input);
+            AddCmdParameter("@detalle", objusuario.Detalle, ParameterDirection.Input);
+            AddCmdParameter("@domicilioCalle", objusuario.DomicilioCalle, ParameterDirection.Input);
+            AddCmdParameter("@domicilioNro", objusuario.DomicilioNro, ParameterDirection.Input);
+            AddCmdParameter("@domicilioDpto", objusuario.DomicilioDpto, ParameterDirection.Input);
+            AddCmdParameter("@domicilioPiso", objusuario.DomicilioPiso, ParameterDirection.Input);
+            AddCmdParameter("@idLocalidad", objusuario.IdLocalidad, ParameterDirection.Input);
+            AddCmdParameter("@email", objusuario.Email, ParameterDirection.Input);
+            AddCmdParameter("@estado", objusuario.Estado, ParameterDirection.Input);
+            AddCmdParameter("@razonSocial", objusuario.RazonSocial, ParameterDirection.Input);
+            AddCmdParameter("@responsableApellido", objusuario.ResponsableApellido, ParameterDirection.Input);
+            AddCmdParameter("@responsableNombre", objusuario.ResponsableNombre, ParameterDirection.Input);
+            AddCmdParameter("@responsableEmail", objusuario.ResponsableEmail, ParameterDirection.Input);
+            AddCmdParameter("@telefono", objusuario.Telefono, ParameterDirection.Input);
+            AddCmdParameter("@nombre", objusuario.Nombre, ParameterDirection.Input);
+        }
+
+        public object Insert(Dyn.Database.entities.Proveedor objProveedor)
+        {
+            object IdProveedor = null;
+            objProveedor.Estado = 1;
+            AddParameters(objProveedor);
+            AddCmdParameter("@Action", 2, ParameterDirection.Input);
             ExecuteReader();
             while (Read())
             {
-                Collection.Add(new Dyn.Database.entities.Genero(GetDataReader()));
+                IdProveedor = GetValue(0);
             }
-            return Collection;
+            return IdProveedor;
+        }
+        public void Update(Dyn.Database.entities.Proveedor objProveedor)
+        {
+            objProveedor.Estado = 1;
+            AddParameters(objProveedor);
+            AddCmdParameter("@Action", 1, ParameterDirection.Input);
+            ExecuteNonQuery();
+        }
+
+        public void Delete(int idProveedor)
+        {
+            CreateCommand("usp_Proveedor", true);
+            AddCmdParameter("@idProveedor", idProveedor, ParameterDirection.Input);
+            AddCmdParameter("@estado", 0, ParameterDirection.Input);
+            AddCmdParameter("@Action", 3, ParameterDirection.Input);
+            ExecuteNonQuery();
         }
 
     }
