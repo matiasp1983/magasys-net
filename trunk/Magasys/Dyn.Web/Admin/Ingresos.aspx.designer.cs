21df66c9331e4bf61288aac2e06f5e962cc9fb14
margacba85@gmail.com <margacba85@gmail.com@267969d2-7f81-b237-fc1d-be3311caf4fb>
2013-08-14 00:28:54 +0000

    Ingreso

--- trunk/Magasys/Dyn.Web/Admin/Ingresos.aspx.designer.cs
+++ trunk/Magasys/Dyn.Web/Admin/Ingresos.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace Dyn.Web.Admin {
     public partial class Ingresos {
         
         /// <summary>
-        /// txtNumeroVenta control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtNumeroVenta;
-        
-        /// <summary>
         /// calFecha control.
         /// </summary>
         /// <remarks>
