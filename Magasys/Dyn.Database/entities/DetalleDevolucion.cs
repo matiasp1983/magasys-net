ffe76d82ddb7794f8ea0678f29091b19c1464e99
matiasp1983 <matiasp1983@gmail.com>
2013-08-16 19:34:11 +0000

    Se comenzo a realizar Devolucion

--- Magasys/Dyn.Database/entities/DetalleDevolucion.cs
+++ Magasys/Dyn.Database/entities/DetalleDevolucion.cs
@@ -1,31 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data;
 using System.Text;
 
 namespace Dyn.Database.entities
 {
-    class DetalleDevolucion
+    public class DetalleDevolucion
     {
         #region Constructores
 
         public DetalleDevolucion() { }
 
-        public DetalleDevolucion(Int32? idDetalleDevol, Int32? idDevol, Int32? IdDetalleIngrProd, Int16? est, Int32? cantid)
+        public DetalleDevolucion(Int32? idDetalleDevol, Int32? idDevol, Int32? IdDetalleIngrProd, Int16? est, Int32? cantid, Producto prod, ProductoEdicion prodEdi)
         {
             idDetalleDevolucion = idDetalleDevol;
             idDevolucion = idDevol;
             IdDetalleIngresoProductos = IdDetalleIngrProd;
             estado = est;
             cantidad = cantid;
+            producto = prod;
+            productoEdicion = prodEdi;
         }
+
+        public DetalleDevolucion(IDataRecord obj)
+		{
+            idDetalleDevolucion = Convert.ToInt32(obj["idDetalleDevolucion"]);
+            idDevolucion = Convert.ToInt32(obj["idDevolucion"]);
+            cantidad = Convert.ToInt32(obj["cantidad"]);
+            idDetalleIngresoProductos = Convert.ToInt32(obj["idDetalleIngresoProductos"]);
+            estado = Convert.ToInt16(obj["estado"]);
+            producto.IdProducto = Convert.ToInt32(obj["idProducto"]);
+            this.productoEdicion.IdProductoEdicion  = Convert.ToInt32(obj["idProductoEdicion"]);
+
+		}
 
         #endregion
 
         #region Propiedades
 
         private Int32? idDetalleDevolucion;
-
         public Int32? IdDetalleDevolucion
         {
             get { return idDetalleDevolucion; }
@@ -33,7 +46,6 @@ namespace Dyn.Database.entities
         }
 
         private Int32? idDevolucion;
-
         public Int32? IdDevolucion
         {
             get { return idDevolucion; }
@@ -41,7 +53,6 @@ namespace Dyn.Database.entities
         }
 
         private Int32? idDetalleIngresoProductos;
-
         public Int32? IdDetalleIngresoProductos
         {
             get { return idDetalleIngresoProductos; }
@@ -49,7 +60,6 @@ namespace Dyn.Database.entities
         }
 
         private Int16? estado;
-
         public Int16? Estado
         {
             get { return estado; }
@@ -57,12 +67,27 @@ namespace Dyn.Database.entities
         }
 
         private Int32? cantidad;
-
         public Int32? Cantidad
         {
             get { return cantidad; }
             set { cantidad = value; }
         }
+
+        private Producto producto;
+        public Producto Producto
+        {
+            get { return producto; }
+            set { producto = value; }
+        }
+
+        private ProductoEdicion productoEdicion;
+        public ProductoEdicion ProductoEdicion
+        {
+            get { return productoEdicion; }
+            set { productoEdicion = value; }
+        }
+
+
 
         #endregion
     }
---
+++ Magasys/Dyn.Database/logic/Devolucion.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Dyn.Database.logic
+{
+    public class Devolucion : FactoryBase
+    {
+        public Devolucion() { }
+
+        public Dyn.Database.entities.Devolucion Load(int idDevolucion)
+        {
+            Dyn.Database.entities.Devolucion objDevolucion = new Dyn.Database.entities.Devolucion();
+            CreateCommand("usp_Devolucion", true);
+            AddCmdParameter("@idDevolucion", idDevolucion, ParameterDirection.Input);
+            AddCmdParameter("@Action", 0, ParameterDirection.Input);
+            ExecuteReader();
+            while (Read())
+            {
+                objDevolucion = new Dyn.Database.entities.Devolucion(GetDataReader());
+            }
+            return objDevolucion;
+        }
+
+        private void AddParameters(Dyn.Database.entities.Devolucion objusuario)
+        {
+            Database.logic.ProductoEdicion2 lProdEdi = new Database.logic.ProductoEdicion2();
+            int idProdEdi = Convert.ToInt32(lProdEdi.Insert(objusuario.ProductoEdicion));
+            CreateCommand("usp_DetalleIngreso", true);
+            AddCmdParameter("@idDetalleIngresoProductos", objusuario.IdDetalleIngresoProducto, ParameterDirection.Input);
+            AddCmdParameter("@idIngresoProductos", objusuario.IdIngresoProductos, ParameterDirection.Input);
+            AddCmdParameter("@cantidadUnidades", objusuario.CantidadUnidades, ParameterDirection.Input);
+            AddCmdParameter("@fechaDevolucion", objusuario.FechaDevolucion, ParameterDirection.Input);
+            AddCmdParameter("@estado", objusuario.Estado, ParameterDirection.Input);
+            AddCmdParameter("@idProducto", objusuario.Producto.IdProducto, ParameterDirection.Input);
+            AddCmdParameter("@idProductoEdicion", idProdEdi, ParameterDirection.Input);
+        }
+
+        public object Insert(Dyn.Database.entities.Devolucion objDetalle)
+        {
+
+            object IdDetalle = null;
+            objDetalle.Estado = 1;
+            AddParameters(objDetalle);
+            AddCmdParameter("@Action", 2, ParameterDirection.Input);
+            ExecuteReader();
+            while (Read())
+            {
+                IdDetalle = GetValue(0);
+            }
+            return IdDetalle;
+        }
+        public void Update(Dyn.Database.entities.Devolucion objDetalle)
+        {
+            objDetalle.Estado = 1;
+            AddParameters(objDetalle);
+            AddCmdParameter("@Action", 1, ParameterDirection.Input);
+            ExecuteNonQuery();
+        }
+
+        public void Delete(int idDetalle)
+        {
+            CreateCommand("usp_DetalleIngreso", true);
+            AddCmdParameter("@idDetalle", idDetalle, ParameterDirection.Input);
+            AddCmdParameter("@estado", 0, ParameterDirection.Input);
+            AddCmdParameter("@Action", 3, ParameterDirection.Input);
+            ExecuteNonQuery();
+        }
+
+    }
+}
