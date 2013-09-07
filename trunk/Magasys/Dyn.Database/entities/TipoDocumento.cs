c264deba71d1a5f1c182d0cbf71cab31969f3f55
matiasp1983@gmail.com <matiasp1983@gmail.com@267969d2-7f81-b237-fc1d-be3311caf4fb>
2013-09-07 22:09:13 +0000

    Se crearon Clases Entidad: Cliente, TipoDocumento y Estado. Se creo ABMCliente.aspx en Web

---
+++ trunk/Magasys/Dyn.Database/entities/TipoDocumento.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Dyn.Database.entities
+{
+    public class TipoDocumento
+    {
+        #region Constructores
+
+        public TipoDocumento() { }
+
+        public TipoDocumento(Int32? id, string nom, string desc)
+        {
+            idTipoDocumento = id;
+            nombre = nom;
+            descripcion = desc;
+        }
+        public TipoDocumento(IDataRecord obj)
+        {
+            idTipoDocumento = Convert.ToInt32(obj["idTipoDocumento"]);
+            nombre = Convert.ToString(obj["nombre"]);
+            descripcion = nombre = Convert.ToString(obj["descripcion"]);
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        private Int32? idTipoDocumento;
+        public Int32? IdTipoDocumento
+        {
+            get { return idTipoDocumento; }
+            set { idTipoDocumento = value; }
+        }
+
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string descripcion;
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        #endregion
+
+    }
+}
