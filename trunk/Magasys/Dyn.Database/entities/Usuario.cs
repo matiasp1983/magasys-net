174c06b03b23de33fc5c970f790bc58e2dc5242b
margacba85@gmail.com <margacba85@gmail.com@267969d2-7f81-b237-fc1d-be3311caf4fb>
2013-10-17 03:49:55 +0000

    Se sube Usuario, falta terminar.

---
+++ trunk/Magasys/Dyn.Database/entities/Usuario.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Dyn.Database.entities
+{
+    public class Usuario
+    {
+        #region Constructores
+
+        public Usuario() { }
+
+        public Usuario(Int32? idUsu, Int32? legajo, string log, string pass, string rl)
+        {
+            idUsuario = idUsu;
+            legajoEmpleado = legajo;
+            login = log;
+            password = pass;
+            rol = rl;
+        }
+
+        public Usuario(IDataRecord obj)
+        {
+            idUsuario = Convert.ToInt32(obj["idUsuario"]);
+            Cliente.NroCliente = Convert.ToInt32(obj["nroCliente"]);
+            legajoEmpleado = Convert.ToInt32(obj["legajoEmpleado"]);
+            Cliente.NroDocumento = Convert.ToInt32(obj["nroDocumento"]);
+            Cliente.TipoDocumento.IdTipoDocumento = Convert.ToInt32(obj["tipoDocumento"]);
+            Cliente.Nombre = Convert.ToString(obj["nombre"]);
+            Cliente.Apellido = Convert.ToString(obj["apellido"]);
+            login = Convert.ToString(obj["login"]);
+            password = Convert.ToString(obj["password"]);
+            rol = Convert.ToString(obj["rol"]);
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        private Int32? idUsuario;
+
+        public Int32? IdUsuario
+        {
+            get { return idUsuario; }
+            set { idUsuario = value; }
+        }
+
+        private Cliente cliente = new Cliente();
+
+        public Cliente Cliente
+        {
+            get { return cliente; }
+            set { cliente = value; }
+        }
+
+        private Int32? legajoEmpleado;
+
+        public Int32? LegajoEmpleado
+        {
+            get { return legajoEmpleado; }
+            set { legajoEmpleado = value; }
+        }
+
+        private string login;
+
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        private string rol;
+
+        public string Rol
+        {
+            get { return rol; }
+            set { rol = value; }
+        }
+
+        private Int16? idEstado;
+
+        public Int16? IdEstado
+        {
+            get { return idEstado; }
+            set { idEstado = value; }
+        }
+        
+        #endregion
+    }
+}
---
+++ trunk/Magasys/Dyn.Database/logic/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Dyn.Database.logic
+{
+    public class Usuario : FactoryBase
+    {
+        public Usuario() {}
+
+        public List<Dyn.Database.entities.Usuario> BuscarUsuarios(string nombre, string apellido, int tipoDoc, int nroDoc)
+        {
+            List<Dyn.Database.entities.Usuario> Collection = new List<Dyn.Database.entities.Usuario>();
+            CreateCommand("usp_Usuario", true);
+            AddCmdParameter("@nombre", nombre, ParameterDirection.Input);
+            AddCmdParameter("@apellido", apellido, ParameterDirection.Input);
+            AddCmdParameter("@tipoDocumento", tipoDoc, ParameterDirection.Input);
+            if (nroDoc != 0)
+            {
+                AddCmdParameter("@nroDocumento", nroDoc, ParameterDirection.Input);
+            }
+            AddCmdParameter("@Action", 0, ParameterDirection.Input);
+            ExecuteReader();
+            while (Read())
+            {
+                Collection.Add(new Dyn.Database.entities.Usuario(GetDataReader()));
+            }
+            return Collection;
+        }
+    }
+}
