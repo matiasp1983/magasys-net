a8c5715f281f42e7ba9e3254f98c873b916dd2bc
matiasp1983 <matiasp1983@gmail.com>
2013-10-17 00:47:38 +0000

    Algunos cambios para el redireccionamiento de las pagina.

---
+++ Magasys/Dyn.WebKiosco/Login.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using Entidades;
+//using DAL;
+using System.Web.Security;
+
+namespace PaginaKiosko
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void BtnConectarse_Click(object sender, EventArgs e) { }
+        //{
+        //    Session["Usuario"] = DAOUsuarios.ValidarUsuario(TxtUsuario.Text, TxtContraseña.Text);
+        //    Usuario user = (Usuario)Session["Usuario"];
+        //    try
+        //    {
+        //        if (user != null)
+        //        {
+        //            String roles;
+        //            switch (user.Rol)
+        //            { 
+        //                case 'A':
+        //                    roles = "ADMIN";
+        //                    break;
+        //                case 'U':
+        //                    roles = "USER";
+        //                    break;
+        //                case 'C':
+        //                    roles = "CLIENT";
+        //                    break;
+        //                default:
+        //                    roles = "ANONIMO";
+        //                    break;
+        //            }
+        //            FormsAuthenticationTicket autTicket = new FormsAuthenticationTicket(1, user.Nombre, DateTime.Now, DateTime.Now.AddMinutes(60), false, roles);
+        //            String encriptedTicket = FormsAuthentication.Encrypt(autTicket);
+        //            HttpCookie autCookie = new HttpCookie(".Kiosko", encriptedTicket);
+        //            Response.Cookies.Add(autCookie);
+        //            // Response.Redirect(FormsAuthentication.GetRedirectUrl(TxtUsuario.Text, false));
+        //            Response.Redirect("Default.aspx");
+                    
+        //        }
+        //        else
+        //        {
+        //            LabInfo.Text = "USUARIO INEXISTENTE O CLAVE INCORRECTA";
+        //            LabInfo.Visible = true;
+                    
+        //        }
+        //    }
+        //        catch (Exception ex)
+        //        {   
+        //            LabInfo.Text = "OCURRIO UN ERROR " + ex.Message;
+        //            LabInfo.Visible = true;
+        //        }
+            
+        //}   
+    }
+}
--- Magasys/Dyn.WebKiosco/mi-kiosco/MiCuenta.aspx
+++ Magasys/Dyn.WebKiosco/mi-kiosco/MiCuenta.aspx
@@ -1,5 +1,96 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MiCuenta.aspx.cs" Inherits="Dyn.WebKiosco.mi_kiosco.MiCuenta" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <style type="text/css">
+    .style1
+    {
+        width: 20px;
+    }
+    .style2
+    {
+        width: 20px;
+        height: 20px;
+    }
+    .style3
+    {
+        height: 20px;
+    }
+</style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <table style="width:100%;">
+    <tr>
+        <td class="style1">
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td class="style1">
+            &nbsp;</td>
+        <td class="art-nav-outer">
+            <h2 class="art-postheader">
+                <span class="art-postheadericon">Mi Cuenta </span>
+            </h2>
+        </td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td class="style2">
+        </td>
+        <td class="style3">
+        </td>
+        <td class="style3">
+        </td>
+        <td class="style3">
+        </td>
+    </tr>
+    <tr>
+        <td class="style1">
+            &nbsp;</td>
+        <td style="font-size: 16px;">
+            Aquí puedas conocer el estado de tu cuenta y las compras realizadas que aun no 
+            han sido abonadas.</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td class="style1">
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td class="style1">
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td class="style1">
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+    </tr>
+</table>
 </asp:Content>
